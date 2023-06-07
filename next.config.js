ec09220020820364afd35ea1897d7e21ac12c381
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-06-07 22:31:11 +0200

    WIP - projetcard component

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
                 protocol: 'https',
                 hostname: 'i.ibb.co',
             },
+            {
+                protocol: 'https',
+                hostname: 'w7.pngwing.com',
+            },
         ],
     },
 };
