ef5f2ad01974c2e9c03f854573c51fa8a72675d8
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-06-10 13:12:47 +0200

    FIX - Incorrect checks on existing tags resulting in duplicate data

--- pages/api/tags.js
+++ pages/api/tags.js
@@ -13,20 +13,25 @@ export default async function handler(req, res) {
 
         // write post request that takes a string as property and adds it to the tags collection
         let { tag } = req.body;
-        tag = tag.toLowerCase();
-        let resp;
+        data.tag = tag.toLowerCase();
+
         // check if tag already exists
+        let resp;
+        let tagExists = false;
         const querySnapshot = await getDocs(collection(firestore, 'tags'));
         querySnapshot.forEach(async (doc) => {
             if (doc.data().tag == tag) {
                 // tag already exists
-                resp = { message: 'Tag already exists' };
-            } else {
-                // save to collection
-                resp = { message: 'Tag added' };
-                const docRef = await addDoc(collection(firestore, 'tags'), data);
+                resp = { message: 'Tag already exists', tag: data };
+                tagExists = true;
             }
         });
+
+        if (!tagExists) {
+            resp = { message: 'Tag added', tag: data };
+            const docRef = await addDoc(collection(firestore, 'tags'), data);
+        }
+
         res.status(200).json(resp);
     } else if (req.method == 'DELETE') {
         // Delete all tags in collection without deleting the collection itself
@@ -43,23 +48,43 @@ export default async function handler(req, res) {
         let { tag, newTag } = req.body;
         tag = tag.toLowerCase();
         newTag = newTag.toLowerCase();
-        let message = '';
-        // check if tag already exists
+
+        let data = {};
+
+        // dynamically add all keys and values from req.body to data object
+        for (let key in req.body) {
+            data[key] = req.body[key];
+        }
+        data.tag = newTag;
+        delete data['newTag'];
+
+        // Check if tag with newTag already exists
+        let tagExists = false;
         const querySnapshot = await getDocs(collection(firestore, 'tags'));
         querySnapshot.forEach(async (doc) => {
-            if (doc.data().tag == tag) {
-                message = 'Tag updated';
+            if (doc.data().tag == newTag) {
                 // tag already exists
-                const docRef = doc.ref;
-                await deleteDoc(docRef);
-                const docRef2 = await addDoc(collection(firestore, 'tags'), { tag: newTag });
-                // success message
-            } else {
-                //error message
-                message = 'Tag does not exist';
+                tagExists = true;
             }
         });
-        res.status(200).json(message);
+
+        if (!tagExists) {
+            // tag does not exist
+            const querySnapshot = await getDocs(collection(firestore, 'tags'));
+            querySnapshot.forEach(async (doc) => {
+                if (doc.data().tag == tag) {
+                    // tag exists
+                    await deleteDoc(doc.ref);
+                    const docRef = await addDoc(collection(firestore, 'tags'), data);
+                }
+            });
+
+            await addDoc(collection(firestore, 'tags'), data);
+        } else {
+            data = { message: 'Tag already exists', tag: data };
+        }
+
+        res.status(200).json(data);
     } else {
         // handle any other HTTP method
         let list = [];
