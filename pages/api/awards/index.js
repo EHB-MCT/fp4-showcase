ddc0148dcbf1812357dd575e922dd3d1a066c250
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-06-12 22:37:26 +0200

    DONE - Get award by ID and update award by ID

--- pages/api/awards/index.js
+++ pages/api/awards/index.js
@@ -1,29 +1,26 @@
-import {firestore} from '../../lib/firebase' 
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDoc, getDocs } from 'firebase/firestore';
+import { firestore } from '../../../lib/firebase';
 
 export default async function handler(req, res) {
-
     if (req.method === 'POST') {
         // Process a POST request
-      } else {
+    } else {
         // Handle any other HTTP method
         //retrieve projects from projects collection
         let list = [];
-        const querySnapshot = await getDocs(collection(firestore, "awards"));
-        querySnapshot.forEach((doc) => {
-          // doc.data() is never undefined for query doc snapshots
-          list.push(doc.data());
-        });
-        res.status(200).json(list)
 
-            
+        const querySnapshot = await getDocs(collection(firestore, 'awards'));
+        querySnapshot.forEach((doc) => {
+            const data = {
+                id: doc.id,
+                ...doc.data(),
+            };
+            list.push(data);
+        });
+
+        res.status(200).json(list);
         // }).catch((error) => {
         //     res.status(500).json({error: error.message})
         // })
-
-
-
-        
-      }
-    
-  }
+    }
+}
