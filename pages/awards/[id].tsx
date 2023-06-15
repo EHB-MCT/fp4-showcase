a93439c1399d95f8df71979ee836e0c5da19bd99
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-06-15 11:53:49 +0200

    WIP - Error fixing for vercel

--- pages/awards/[id].tsx
+++ pages/awards/[id].tsx
@@ -36,7 +36,7 @@ export default function Award() {
   const [projectSelected, setProjectSelected] = useState();
   // user
   const { user } = useContext(UserContext);
-  const [userData, setUserData] = useState();
+  const [userData, setUserData] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isWithdrawModalOpen, setIsWithdrawModalOpen] = useState(false);
   // select a project
@@ -238,7 +238,7 @@ export default function Award() {
           hasParticipated && (
             <div className="bg-black  p-2 w-fit mt-3 opacity-50">
               <p className="font-light ml-2">
-                You are participating with project
+                You are participating with project{" "}
                 <span className="text-fuchsia-600">
                   &#34;
                   {projects.find((project) => project.awardId === id)?.title}
