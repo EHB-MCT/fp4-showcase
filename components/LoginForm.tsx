9da46d432be0456c5162af61123b106e2f881f74
IlhanKurultay <91081941+IlhanKurultay@users.noreply.github.com>
2023-06-15 11:34:03 +0200

    FIX button Pink and button to go back to login.

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -46,7 +46,7 @@ const LoginForm = () => {
                 className="w-2/2 flex flex-col gap-4 items-cente p-5 rounded-xl mb-5 mt-5 border-2 border-purple-500"
 >
                 {mode === "register" ? (
-                    <h1 className="text-center text-white text-2xl">Register User</h1>
+                    <h1 className="text-center text-white text-2xl">Register</h1>
                 ) : (
                     <h1 className="text-center text-white text-2xl">Log in</h1>
                 )
@@ -114,14 +114,14 @@ const LoginForm = () => {
     </div>
 
                    <ButtonPink title="Submit" color='white' />
-                <button 
-                    type="button"
-                    
-                    onClick={() => setMode(mode === "login" ? "register" : "login")}
-                    className="bg-yellow-500 text-white px-4 py-2 rounded-md mt-4 w-full"
-                >
-                    Switch to {mode === "login" ? "register" : "login"}
-                </button>
+<button
+  type="button"
+  onClick={() => setMode(mode === "login" ? "register" : "login")}
+  className="bg-transparent text-white px-4 py-2 rounded-md mt-4 w-full hover:underline"
+>
+  Switch to {mode === "login" ? "register" : "login"}
+</button>
+
 
             </form>
 
