d14dedaf484eea081962459d95d1b0cc4f7a2f4c
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-06-16 09:28:48 +0200

    FIX - New navigation without mobile

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,17 +1,19 @@
+import {
+  faAddressCard,
+  faArrowRightFromBracket,
+  faCaretDown,
+  faCaretUp,
+  faPlus,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect, useState } from "react";
 import { logoutUser } from "../lib/auth";
 import { UserContext } from "../lib/context";
 import style from "../styles/Navigation.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCaretDown,
-  faUser,
-  faAddressCard,
-  faPlus,
-  faArrowRightFromBracket,
-} from "@fortawesome/free-solid-svg-icons";
+import NavigationDropdown from "./NavigationDropdown";
 
 const Navigation: React.FC = () => {
   const router = useRouter();
@@ -21,7 +23,55 @@ const Navigation: React.FC = () => {
   const [username, setUsername] = useState("");
   const [isOpen, setIsOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsOpen(false);
+  };
+
+  const handleSignOut = () => {
+    logoutUser();
+    closeDropdown();
+  };
+
+  const navDropDownItems = [
+    {
+      title: "View Profile",
+      href: user ? `/profile/${user.uid}` : `/`,
+      icon: {
+        name: faAddressCard,
+        color: "#ffffff",
+        style: { width: 20, height: 20 },
+      },
+      closeDropdown: closeDropdown,
+      viewableFor: "student",
+    },
+    {
+      title: "Add Project",
+      href: `/projects/upload`,
+      icon: {
+        name: faPlus,
+        color: "#ffffff",
+        style: { width: 20, height: 20 },
+      },
+      closeDropdown: closeDropdown,
+      viewableFor: "student",
+    },
+    {
+      title: "Sign Out",
+      href: `/`,
+      icon: {
+        name: faArrowRightFromBracket,
+        color: "#ffffff",
+        style: { width: 20, height: 20 },
+      },
+      closeDropdown: handleSignOut,
+      viewableFor: "all",
+    },
+  ];
 
   useEffect(() => {
     if (user) {
@@ -34,6 +84,7 @@ const Navigation: React.FC = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        if (user?.uid == undefined || user?.uid == null) return;
         const res = await fetch(`/api/users/${user.uid}`);
         const data = await res.json();
         setUserObject(data);
@@ -44,22 +95,6 @@ const Navigation: React.FC = () => {
     };
     fetchData();
   }, [user]);
-
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const closeDropdown = () => {
-    setIsOpen(false);
-  };
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
-  const closeMobileMenu = () => {
-    setIsMobileMenuOpen(false);
-  };
 
   const renderLoggedInState = () => {
     if (username && isLoggedIn) {
@@ -74,8 +109,7 @@ const Navigation: React.FC = () => {
             {username}
             {isOpen && (
               <FontAwesomeIcon
-                icon={faCaretDown}
-                rotation={180}
+                icon={faCaretUp}
                 color="#ffffff"
                 style={{ width: 20, height: 20 }}
               />
@@ -89,52 +123,7 @@ const Navigation: React.FC = () => {
             )}
           </div>
           {isOpen && (
-            <div className={style.dropdown_wrapper}>
-              <Link href={`/profile/${user.uid}`}>
-                <a onClick={closeDropdown}>
-                  <FontAwesomeIcon
-                    icon={faAddressCard}
-                    color="#ffffff"
-                    style={{ width: 20, height: 20 }}
-                  />
-                  View Profile
-                </a>
-              </Link>
-              <Link href="/projects/upload">
-                <a
-                  className={
-                    currentRoute === "/register" ? `${style.active}` : ""
-                  }
-                  onClick={closeDropdown}
-                >
-                  <FontAwesomeIcon
-                    icon={faPlus}
-                    color="#ffffff"
-                    style={{ width: 20, height: 20 }}
-                  />
-                  Add Project
-                </a>
-              </Link>
-              <Link href="/register">
-                <a
-                  className={
-                    currentRoute === "/register" ? `${style.active}` : ""
-                  }
-                  onClick={() => {
-                    closeDropdown();
-                    logoutUser();
-                  }}
-                >
-                  <FontAwesomeIcon
-                    icon={faArrowRightFromBracket}
-                    flip={"horizontal"}
-                    color="#ffffff"
-                    style={{ width: 20, height: 20 }}
-                  />
-                  Sign out
-                </a>
-              </Link>
-            </div>
+            <NavigationDropdown items={navDropDownItems} user={userObject} />
           )}
         </div>
       );
@@ -148,6 +137,14 @@ const Navigation: React.FC = () => {
               Log in
             </a>
           </Link>
+          {/* <span className={style.divider}>/</span>
+            <Link href="/register">
+              <a
+                className={currentRoute === "/register" ? `${style.active}` : ""}
+              >
+                Sign up
+              </a>
+            </Link> */}
         </div>
       );
     }
@@ -155,94 +152,49 @@ const Navigation: React.FC = () => {
 
   return (
     <>
-      <div
-        className={`${style.navigation} max-w-7xl mx-auto px-4 sm:px-6 lg:px-8`}
-      >
-        <Link href="/">
-          <svg
-            onClick={closeDropdown}
-            className={style.logo}
-            xmlns="http://www.w3.org/2000/svg"
-            width="111.443"
-            height="61.413"
-            viewBox="0 0 111.443 61.413"
-          >
-            <path
-              id="Path_1253"
-              data-name="Path 1253"
-              d="M65,0V.009l-.009,0L39.808,45.071,14.163,30.687,46.442,12.616V0L.208,25.895,0,26.011V35.4L46.033,61.179l.407.234,24.9-45.24L97.281,30.72,65.21,48.675,65,48.792V61.408l46.234-25.895.208-.116V26.011L76.653,6.525l.012-.021L67.8,1.569Z"
-              transform="translate(0)"
-              fill="#fff"
-              opacity="0.998"
-            />
-          </svg>
-        </Link>
-        <div className={style.links}>
-          <div className={style.cut}></div>
+      <div className={style.navigation_wrapper}>
+        <div className={`${style.navigation} containerWidth`}>
           <Link href="/">
-            <a
+            <svg
               onClick={closeDropdown}
-              className={currentRoute === "/" ? `${style.active}` : ""}
+              className={style.logo}
+              xmlns="http://www.w3.org/2000/svg"
+              width="111.443"
+              height="61.413"
+              viewBox="0 0 111.443 61.413"
             >
-              Home
-            </a>
+              <path
+                id="Path_1253"
+                data-name="Path 1253"
+                d="M65,0V.009l-.009,0L39.808,45.071,14.163,30.687,46.442,12.616V0L.208,25.895,0,26.011V35.4L46.033,61.179l.407.234,24.9-45.24L97.281,30.72,65.21,48.675,65,48.792V61.408l46.234-25.895.208-.116V26.011L76.653,6.525l.012-.021L67.8,1.569Z"
+                transform="translate(0)"
+                fill="#fff"
+                opacity="0.998"
+              />
+            </svg>
           </Link>
-          <Link href="/awards">
-            <a
-              onClick={closeDropdown}
-              className={currentRoute === "/awards" ? `${style.active}` : ""}
-            >
-              Awards
-            </a>
-          </Link>
-          <Link href="/about">
-            <a
-              onClick={closeDropdown}
-              className={currentRoute === "/about" ? `${style.active}` : ""}
-            >
-              About
-            </a>
-          </Link>
-          {renderLoggedInState()}
-        </div>
-        <div className={style.mobile_menu_toggle} onClick={toggleMobileMenu}>
-          <div className={style.hamburger_icon}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </div>
-      </div>
-      {isMobileMenuOpen && (
-        <div className={style.mobile_menu}>
-          <div className={style.mobile_menu_links}>
-            <div
-              style={{ color: "black" }}
-              className={style.mobile_menu_close}
-              onClick={closeMobileMenu}
-            >
-              X
-            </div>
+          <div className={style.links}>
+            <div className={style.cut}></div>
             <Link href="/">
               <a
-                onClick={closeMobileMenu}
-                style={{ color: "black", paddingBottom: "4vh" }}
+                onClick={closeDropdown}
+                className={currentRoute === "/" ? `${style.active}` : ""}
               >
                 Home
               </a>
             </Link>
             <Link href="/awards">
               <a
-                onClick={closeMobileMenu}
-                style={{ color: "black", paddingBottom: "4vh" }}
+                onClick={closeDropdown}
+                className={currentRoute === "/awards" ? `${style.active}` : ""}
               >
                 Awards
               </a>
             </Link>
             <Link href="/about">
               <a
-                onClick={closeMobileMenu}
-                style={{ color: "black", paddingBottom: "4vh" }}
+                onClick={closeDropdown}
+                className={currentRoute === "/about" ? `${style.active}` : ""}
               >
                 About
               </a>
@@ -250,7 +202,7 @@ const Navigation: React.FC = () => {
             {renderLoggedInState()}
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
