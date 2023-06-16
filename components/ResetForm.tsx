492419ca53730db03136de13a7d423de8d666dc4
mikederycke <mike.derycke@ehb.be>
2023-06-16 11:40:16 +0200

    Working version pwreset

--- components/ResetForm.tsx
+++ components/ResetForm.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useContext, useState } from "react";
 import { UserContext } from "../lib/context";
-import { registerUser, loginUser } from "../lib/auth";
+import { registerUser, loginUser, passwordReset } from "../lib/auth";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/router";
 import ButtonPink from "./ButtonPink";
@@ -12,16 +12,24 @@ const onClick = () => {
 
 const ResetForm = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const { user } = useContext(UserContext);
-  const router = useRouter();
   const [mode, setMode] = useState<"reset" | "reauthenticate">("reset");
+  const [emailMessage, setEmailMessage] = useState(true);
 
   const onSubmit = async (e) => {
     e.preventDefault();
     let user = null;
     if (mode === "reset") {
-      
+      try {
+        let data = await passwordReset(email);
+        console.log(data);
+        setEmailMessage(true);
+      } catch (error:any) {    
+        if (error.code === 'auth/user-not-found') {
+          alert('User not found, try again!')
+          setEmail('')
+        }
+      }
+
     } else {
       
     }
@@ -38,6 +46,14 @@ const ResetForm = () => {
 
   return (
     <>
+      {emailMessage ? <div className="w-2/2 flex flex-col gap-4 items-cente p-5 rounded-xl mb-5 mt-5 border-2 border-purple-500"
+     > 
+        <h1 className="text-center text-white text-2xl">The Email has been sent. </h1>
+        <hr className="h-px my-3 bg-gray-200 border-0 w-full "></hr>
+        <div className="flex flex-col gap-2 items-start w-full"></div>
+        <h2>Check your Inbox!</h2>
+      </div>  : (
+
       <form
         onSubmit={onSubmit}
         className="w-2/2 flex flex-col gap-4 items-cente p-5 rounded-xl mb-5 mt-5 border-2 border-purple-500"
@@ -82,48 +98,13 @@ const ResetForm = () => {
           />
         </div>
 
-        {/* <label className="text-white" htmlFor="password">
-          Password:
-        </label>
-        <div style={{ position: "relative" }}>
-          <svg
-            width="100%"
-            height="50"
-            viewBox="0 0 570 50"
-            preserveAspectRatio="none"
-          >
-            <path
-              id="Path_1213"
-              data-name="Path 1213"
-              d="M1618.957,5931.394v-27.36l-18.019-13.388-538.938,1.6v27.544l17.982,11.6Z"
-              transform="translate(-1061 -5889.645)"
-              fill="#202033"
-              stroke="#fff"
-              strokeWidth="2"
-              opacity="0.66"
-            />
-          </svg>
-          <input
-            type="password"
-            placeholder="Password"
-            id="passwordInput"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            style={{ position: "absolute", top: "1px", width: "80%" }}
-            className="border-none p-2 w-full rounded-sm bg-transparent text-white mx-4"
-            required
-          />
-        </div> */}
+       
 
         <ButtonPink title="Submit" color="white" onClick={onClick} />
-        {/* <button
-          type="button"
-          onClick={() => setMode(mode === "login" ? "register" : "login")}
-          className="bg-yellow-500 text-white px-4 py-2 rounded-md mt-4 w-full"
-        >
-          Switch to {mode === "login" ? "register" : "login"}
-        </button> */}
-      </form>
+       
+        </form>
+      
+      )}
     </>
   );
 };
