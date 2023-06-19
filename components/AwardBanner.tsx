775cd3423a922ca97f01bbb877687c27a88c5f20
Stephan Van Hemelrijck <stephan.van.hemelrijck@student.ehb.be>
2023-06-19 18:05:05 +0200

    FIX - Styling issue op award page

--- components/AwardBanner.tsx
+++ components/AwardBanner.tsx
@@ -2,24 +2,28 @@ import React from "react";
 import styles from "../styles/Awards.module.css";
 import DottedLine from "./DottedLine";
 
-const AwardBanner = ({award}) => {
+const AwardBanner = ({ award }) => {
   if (!award) {
     return null; // Render nothing if the award object is null
   }
-    return (
-      <div className={styles.bannerAward}>
-        <div className={styles.bannerImage}>
-          <div className={styles.bannerText}>
-            <div className={styles.content}>
-              <h1>{award.title}</h1>
-              <DottedLine />
-              <p>
-    {award.description}              </p>
-            </div>
+  return (
+    <div className={styles.bannerAward}>
+      <div
+        className={styles.bannerImage}
+        style={{
+          backgroundImage: `url(${award.bannerImageUrl})`,
+        }}
+      >
+        <div className={styles.bannerText}>
+          <div className={styles.content}>
+            <h1>{award.title}</h1>
+            <DottedLine />
+            <p>{award.description} </p>
           </div>
         </div>
       </div>
-    );
-  };
-  
-  export default AwardBanner;
+    </div>
+  );
+};
+
+export default AwardBanner;
