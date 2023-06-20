43ea0ae6a7ceb1f0c616874af28dc0e92b512c88
IlhanKurultay <91081941+IlhanKurultay@users.noreply.github.com>
2023-06-20 22:26:02 +0200

    some home page changes with small fixes

--- components/FilterComponent.tsx
+++ components/FilterComponent.tsx
@@ -195,45 +195,51 @@ const FilterComponent = ({
           </div>
         ))}
       </div>
-      <div className={styles.mobile_filter_toggle} onClick={toggleMobileFilter}>
+      <div className={styles.mobile_filter_toggle}>
         <div className={styles.mobile_filter_margin}>
-          <h3>Filter</h3>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="36"
-            height="36"
-            viewBox="0 0 36 36"
-          >
-            <path
-              id="Icon_awesome-filter"
-              data-name="Icon awesome-filter"
-              d="M34.311,0H1.689A1.689,1.689,0,0,0,.5,2.881l13,13.006V30.375a1.688,1.688,0,0,0,.72,1.382l5.625,3.936A1.689,1.689,0,0,0,22.5,34.311V15.886l13-13.006A1.689,1.689,0,0,0,34.311,0Z"
-              fill="#fff"
-            />
-          </svg>
+          <div className={styles.filterSearchContainer}>
+            <form onSubmit={handleSearchSubmit}>
+              <input
+                type="text"
+                placeholder="Project title"
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+              <button type="submit" className={styles.searchButton}>
+                <span className="material-icons">search</span>
+              </button>
+            </form>
+          </div>
+          <div className={styles.mobilefilterIcon} onClick={toggleMobileFilter}>
+            <p>Filter</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="36"
+              height="36"
+              viewBox="0 0 36 36"
+            >
+              <path
+                id="Icon_awesome-filter"
+                data-name="Icon awesome-filter"
+                d="M34.311,0H1.689A1.689,1.689,0,0,0,.5,2.881l13,13.006V30.375a1.688,1.688,0,0,0,.72,1.382l5.625,3.936A1.689,1.689,0,0,0,22.5,34.311V15.886l13-13.006A1.689,1.689,0,0,0,34.311,0Z"
+                fill="#fff"
+              />
+            </svg>
+          </div>
         </div>
       </div>
       {isMobileFilterOpen && (
         <div className={styles.mobile_filter_toggle_style}>
-          <div className={styles.filterContainerMob}>
+          <div
+            style={{ marginTop: "12%" }}
+            className={styles.filterContainerMob}
+          >
             <div style={{ width: "70%", margin: "auto" }}>
               <p style={{ textAlign: "right" }} onClick={closeMobileFilter}>
                 X
               </p>
             </div>
-            <div className={styles.filterSearchContainer}>
-              <form onSubmit={handleSearchSubmit}>
-                <input
-                  type="text"
-                  placeholder="Project title"
-                  value={searchQuery}
-                  onChange={handleSearchChange}
-                />
-                <button type="submit" className={styles.searchButton}>
-                  <span className="material-icons">search</span>
-                </button>
-              </form>
-            </div>
+
             <div className={styles.filterTagsContainer}>
               <DropdownComponent
                 options={tags}
